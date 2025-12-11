37bb4e6a198f82f343c88698792d06a91eddeb4b
JANIER-VSQZ <vasquezjanier31@gmail.com>
2025-12-10 22:14:13 -0600

    mas configuraciones de factura

--- src/Controllers/Factura/Factura.php
+++ src/Controllers/Factura/Factura.php
@@ -8,27 +8,33 @@ use Dao\Facturas\DetallesFactura;
 use Dao\Dao;
 use Views\Renderer;
 use Utilities\Site;
+use Utilities\Security;
+use Dao\Usuario\Usuario;
+use Dao\Security\UsuarioTipo;
 
 class Factura extends PublicController{
     public function run():void{
         Site::addLink("public/css/factura.css");
         $idFactura = intval($_GET["idFactura"]);
         $idUsuario = intval($_GET["idUsuario"]);
+        $idUsuarioLogged=Security::getUserId();
         $dataview = [];
         $impuesto=0;
         $subtotal=0;
+        $usuario=Usuario::obtenerUsuarioPorCodigo($idUsuarioLogged);
 
-        if($idUsuario === $_SESSION["login"]["userId"]){
+
+        if($idUsuario === $idUsuarioLogged || $usuario["usertipo"]===UsuarioTipo::AUDITOR || $usuario["usertipo"]===UsuarioTipo::ADMINITRADAOR){
             $factura = facturasDao::obtenerFacturasPorCodigo($idFactura, $idUsuario);
             $detalles = DetallesFactura::obtenerDetalles($idFactura);
             $dataview ["facturaCorrecta"] = true;
             $dataview ["orderId"] = $factura["orderIdPaypal"];
-            $dataview["fechaFactura"] = $factura["fechaFactura"];
-            $dataview["nombreUsuario"] = $factura["username"];
-            $dataview["nombreCliente"] = $factura["nombreCliente"] . " " . $factura["apellidoCliente"];
-            $dataview["email"] = $_SESSION["login"]["userEmail"];
-            $dataview["impuestoFactura"]=0;
-            $dataview["subtotalFactura"]=0;
+            $dataview ["fechaFactura"] = $factura["fechaFactura"];
+            $dataview ["nombreUsuario"] = $factura["username"];
+            $dataview ["nombreCliente"] = $factura["nombreCliente"] . " " . $factura["apellidoCliente"];
+            $dataview ["email"] = $_SESSION["login"]["userEmail"];
+            $dataview ["impuestoFactura"]=0;
+            $dataview ["subtotalFactura"]=0;
 
                 foreach($detalles as $detalle){
                     $dataview["detalles"][] = [
