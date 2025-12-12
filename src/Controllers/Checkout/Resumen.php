198d263b988b3a7c6e919ae69d8a9c9e0fd337a4
JuanRa <jraguilar012012@gmail.com>
2025-12-11 18:40:21 -0600

    Stilos
    Aplicaciones de estilos al cart

--- src/Controllers/Checkout/Resumen.php
+++ src/Controllers/Checkout/Resumen.php
@@ -7,11 +7,13 @@ use Utilities\Security;
 use Dao\Carretilla\Carretilla as DaoCarretilla;
 use Dao\Carretilla\CarretillaAnon as DaoCarretillaAnon;
 use Views\Renderer;
+use Utilities\Site;
 
 class Resumen extends PrivateController
 {
     public function run(): void
     {
+        Site::addLink("public/css/lists.css");
         $items = [];
         if (Security::isLogged()) {
             $items = DaoCarretilla::getAllByUser(Security::getUserId());
